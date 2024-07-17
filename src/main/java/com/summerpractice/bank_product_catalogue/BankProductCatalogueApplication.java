23861a6c878f84b8fbd10e3eb86d00e2a4bb26f2
Георги <georgimracenkov009@gmail.com>
2024-07-17 12:09:51 +0300

    initial commit

---
+++ src/main/java/com/summerpractice/bank_product_catalogue/BankProductCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.summerpractice.bank_product_catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProductCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankProductCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summerpractice/bank_product_catalogue/BankProductCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerpractice.bank_product_catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProductCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
