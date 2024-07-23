a20a8f378ee25899f97814e74d596e7462b9ed8f
iraychev <ivoraychev1337@gmail.com>
2024-07-23 11:26:11 +0300

    validate clientnumber on login, save client in localstorage

--- src/main/resources/static/js/clientLogin.js
+++ src/main/resources/static/js/clientLogin.js
@@ -1,10 +1,52 @@
-console.log("here")
+import {get} from "./requester";
 
+const submitBtn = document.getElementById('submitBtn');
 const landingButton = document.getElementById('landing-page-login-button')
-console.log(landingButton)
-landingButton.addEventListener('click', function() {
-    console.log("inside")
-    const clientNumber = document.getElementById('customer-number-input-field').value;
-    console.log(clientNumber)
-    localStorage.setItem('clientNumber', clientNumber);
+
+landingButton.addEventListener('click', async function () {
+
+    await fetchAndSaveClient()
 });
+
+let client;
+async function fetchAndSaveClient() {
+    try {
+        const inputNumber = document.getElementById('customer-number-input-field').value;
+
+        if(inputNumber.length() < 9 && inputNumber.length() >10) {
+            alert("Invalid length. Enter a valid EGN or Client number.")
+        }
+        if(inputNumber.length() === 9){
+            client = await get(`clients/v1.0.0/get/client-number/${inputNumber}`);
+        }
+        if(inputNumber.length() === 10){
+            client = await get(`clients/v1.0.0/get/EGN/${inputNumber}`);
+        }
+
+
+        if(!client){
+            popup.style.display = 'block';
+            return;
+        }
+        window.location.href="./ext/landing-page.html";
+
+        localStorage.setItem('client', client)
+    } catch (error) {
+        console.error('Error loading users:', error);
+    }
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const inputEGNValue = document.getElementById('EGN').value;
+    const inputNameValue = document.getElementById('Name').value;
+    const inputEmailValue = document.getElementById('email').value;
+
+    client.EGN = inputEGNValue;
+    client.names = inputNameValue;
+    client.email = inputEmailValue;
+
+
+    window.location.href="./ext/landing-page.html";
+});
+
