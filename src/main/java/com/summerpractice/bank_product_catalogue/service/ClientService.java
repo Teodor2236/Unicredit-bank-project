16f9a814f343924548fedd12d2a5bb565290dec5
iraychev <ivoraychev1337@gmail.com>
2024-07-23 14:51:07 +0300

    remove unused imports

--- src/main/java/com/summerpractice/bank_product_catalogue/service/ClientService.java
+++ src/main/java/com/summerpractice/bank_product_catalogue/service/ClientService.java
@@ -4,8 +4,6 @@ import com.summerpractice.bank_product_catalogue.model.DTO.ClientDTO;
 import com.summerpractice.bank_product_catalogue.model.entity.Client;
 import com.summerpractice.bank_product_catalogue.repository.ClientRepository;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
