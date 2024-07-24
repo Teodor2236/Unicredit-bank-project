1619a946f6f4679d1316c67906a0f413d307a258
iraychev <ivoraychev1337@gmail.com>
2024-07-24 13:39:07 +0300

    update mortgage, consumer credit pages to be filled out with data from the api

--- src/main/resources/static/js/consumer-credit.js
+++ src/main/resources/static/js/consumer-credit.js
@@ -1,7 +1,29 @@
+import {get} from "../js/requester.js";
+
 let menu = document.querySelector("#navbar");
 let nav = document.querySelector(".menu");
+const consumerContainer = document.getElementById('consumer-container')
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   nav.classList.toggle("active");
 };
+
+async function loadConsumerDetails(){
+  try{
+    const consumer = await get('loans/v1.0.0/get/consumer');
+
+    let consumerHTML = '';
+
+    consumerHTML +=
+        `
+        <img src="${consumer.imageUrl}" alt="">
+        <p> ${consumer.description} </p>
+        `
+    consumerContainer.innerHTML += consumerHTML;
+  }
+  catch (e){
+    alert(`Error: ${e}`)
+  }
+}
+await loadConsumerDetails()
